0aaf6565630854371e338a0a4bdc30a67d6af35b
Marwan Harb <marwancharb@gmail.com>
2018-02-19 16:32:42 +0200

    INITIAL COMMIT

---
+++ bitrise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bitrise
+//
+//  Created by Marwan Harb on 2/19/18.
+//  Copyright © 2018 Apps and Games Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bitrise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bitrise
+//
+//  Created by Marwan Harb on 2/19/18.
+//  Copyright © 2018 Apps and Games Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
